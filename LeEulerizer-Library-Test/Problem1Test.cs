e2b5dd06a8ad92c95c114e4f4f542a04fef77c23
Ryan Ransford <rdransfo@gmail.com>
2013-08-18 20:59:55 -0400

    Problem1 class can be constructed

---
+++ LeEulerizer-Library-Test/Problem1Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeEulerizer_Library;
+
+namespace LeEulerizer_Library_Test
+{
+    [TestClass]
+    public class Problem1Test
+    {
+        [TestMethod]
+        public void Problem1_canBeConstructed()
+        {
+            Assert.IsNotNull(new Problem1());
+        }
+    }
+}
