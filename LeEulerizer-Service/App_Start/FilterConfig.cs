72464d1b3f295a7e90f235f7a69fa7e895eca4da
Ryan Ransford <rdransfo@gmail.com>
2013-08-19 12:46:10 -0400

    Adding a web service for problem solutions

---
+++ LeEulerizer-Service/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LeEulerizer_Service
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
